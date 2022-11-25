31c451464a419d83aa840c7b57ee54d82305ee48
Fábio T.G <52431226+fabiomazga@users.noreply.github.com>
2022-11-25 09:42:12 -0300

    Update index.js

--- src/pages/ToDo/index.js
+++ src/pages/ToDo/index.js
@@ -1,218 +1,190 @@
 
-import React from 'react';
-import { SafeAreaView, StyleSheet, View, Text, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
-import ICON from 'react-native-vector-icons/MaterialIcons';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-const COLORS = { primary: '#BE2444', secondary: '#F2F2F2', white: '#fff' };
+import React, {useState} from 'react';
+import { StyleSheet, View, StyleView, Text, ScrollView, TouchableOpacity, Modal } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+
+export default function ToDo() {
+
+    const [tarefas, setTarefas] = useState(['app']); 
+    const [text, setText] = useState("");
+  //dia e hora
+  var hoje = new Date();
+  var horaData = hoje.getHours() + ':' + hoje.getMinutes() + ' ' + hoje.getDate() + '/' + (hoje.getMonth()+1) + '/' + hoje.getFullYear();
 
 
-const ToDo = () => {
-    const [textInput, setTextInput] = React.useState('');
-    const [tarefas, setTarefas] = React.useState([]);
-   React.useEffect(()=>{getTarefasFromUserDevice();},[]);
-   React.useEffect(()=>{saveTarefaToUserDevice(tarefas);},[tarefas]);
 
-   const adicionarTarefa = () => { 
-    if (textInput == '') {
-        Alert.alert('Erro', 'Por favor, digite uma tarefa');
 
-    } else {
 
-        const newTarefa = {
-            id: Math.random(),
-            task: textInput,
-            completed: false,
-        };
-
-        setTarefas([...tarefas, newTarefa]);
-        setTextInput('');
-    }   
-};
-
-const saveTarefaToUserDevice = async tarefas =>{
-    try{
-        const stringfyTarefas = JSON.stringify(tarefas);
-        await AsyncStorage.setItem('tarefas', stringfyTarefas);
-    } catch (error){
-        console.log(error);
+//adicionar tarefa
+  const Adicionar = () => {
+    if(text == ""){
+        alert("Digite uma tarefa");
     }
-};
+    else{
+    setTarefas([...tarefas, text])         
+    setText(" ") }
 
-const getTarefasFromUserDevice = async () => {
-    try{
-        const Tarefas = await AsyncStorage.getItem('tarefas');
-        if (Tarefas != null){
-            setTarefas(JSON.parse(Tarefas));
-        }
-    } catch (error){
-        console.log(error);
+  }
+
+
+//editar tarefa
+  const Editar = (index) => {
+    setText(tarefas[index])
+    tarefas.splice(index, 1)
+    setTarefas([...tarefas])
+  }
+
+//marcar tarefa completa
+const Completa = (index) => {
+    setText(tarefas[index])
+    tarefas.splice(index, 1)
+    setTarefas([...tarefas])
+  }
+
+//excluir tarefa                                       
+    const Excluir = (index) => {
+    tarefas.splice(index, 1)                                     
+    setTarefas([...tarefas])
     }
-};
-
-const marcarTarefaCompleta = tarefaId => {
-    
-    const newTarefasItem = tarefas.map(item => {
-        if (item.id == tarefaId) {
-            return { ...item, completed: true }
-        }
-        return item;
-    });
-    setTarefas(newTarefasItem);
-
-};
-
-const deleteTarefa = tarefaId => {
-    const newTarefasItem = tarefas.filter(item => item.id != tarefaId);
-    setTarefas(newTarefasItem);
-    
-};
-
-const limparTodasTarefas = () => {
-    Alert.alert('Deseja', 'Apagar todas as tarefas?', [
-        {
-            text: 'Sim',
-            onPress: () => setTarefas([]),
-        },
-        {
-            text: 'Não',
-        },
-
-     ]);
-};
 
 
-const ListItem = ({ tarefa }) => {
-                                    return (
-                                        <View style={styles.listItem}>
-                                            <View style={{ flex: 1 }}>
-                                                <Text style={{
-                                                    fontWeight: 'none', fontSize: 20, color: COLORS.white,
-                                                    textDecorationLine: tarefa?.completed ? 'line-through' : 'none',
-                                                }}>
-                                                    {tarefa?.task}
-                                                </Text>
-                                            </View>
 
-                                            {!tarefa?.completed && (
-                                                    <TouchableOpacity onPress={()=> marcarTarefaCompleta(tarefa.id)}>
-                                                        <View style={[styles.actionICON,{backgroundColor:'white'}]}>
-                                                        <ICON name="done" size={20} color="white"/>
-                                                        </View>
-                                                    </TouchableOpacity>
 
-                                                )}
+//limpar tarefas
+    const Limpar = () => {
+        setTarefas([])
 
-                                                    <TouchableOpacity onPress={()=> deleteTarefa(tarefa.id)}>
-                                                        <View style={[styles.actionICON]}>
-                                                        <ICON name="delete" size={20} color="white"/>
-                                                        </View>
-                                                    </TouchableOpacity>
-                                        </View>
-                                    );
-                              }; 
+    }
+    return (
+        <View style = {styles.Tarefa_Container}> 
+
+            <View style={styles.Tarefa_Header}>
+                <Text style={styles.Tarefa_Header_Text}>Lista de Tarefas</Text>
+            </View>
+            <View style={styles.Tarefa_Body}>
+                <TextInput style={{backgroundColor:'ffffff', color:'white',height:55}} label={'Digite a descrição da tarefa:'} value={text} onChangeText={text => setText(text)}/>
+                
 
 
 
 
 
 
+                <View style={styles.Body}>
+                    <Button style={{marginTop: 10, with:"48.5", color:'white', marginRight:5,flex:1,borderRadius:20}} mode="contained" onPress={() => setTarefas([...tarefas, text])}
+                    mode='contained'
+                    color={'#BE2444'}
+                    onPress={Adicionar}
+                    >
+                        Adicionar
+                    </Button>
+                    <Button style={{marginTop: 10, with:"48.5", color:'white', marginLeft:5,flex:1,borderRadius:20}} mode="contained" onPress={() => setTarefas([...tarefas, text])}
+                    mode='contained'
+                    color={'#BE2444'}
+                    onPress={Limpar}
+                    >
+                        Deletar todos
+                    </Button>
 
-return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
-        <View style={styles.header}>
-            <Text style={{ fontWeight: 'bold', fontSize: 25, color: COLORS.primary }}>
-                Tarefas Tech4Fight App
-            </Text>
-            <ICON name="delete" size={30} color={COLORS.primary} onPress={limparTodasTarefas}/>
-        </View>
+                </View>
 
-        <FlatList
-            showsVerticalScrollIndicator={false}
-            contentContainerStyle={{ padding: 20, paddingBottom: 100 }}
-            data={tarefas}
-            renderItem={({ item }) => <ListItem tarefa={item} />}
-        />
-        <View style={styles.footer}>
-            <View style={styles.inputContainer}>
-                <TextInput
-                    placeholder="Incluir Tarefa"
-                    value={textInput}
-                    onChangeText={text => setTextInput(text)}
-                />
             </View>
 
 
-            <TouchableOpacity onPress={adicionarTarefa}>
-                <View style={styles.ICONContainer}>
-                    <ICON name="add" size={50} color="white"/>
-                </View>
-            </TouchableOpacity>
+
+
+            <ScrollView>
+                {
+                tarefas.map((data, index) => {
+                 return <TouchableOpacity activeOpacity={.7} style={styles.Tarefa_Footer} key={index}>
+                    <Text styel={styles.Text}><Text>Tarefa {index+1} - </Text></Text>
+                    <Text style={styles.Text}>{data}</Text>
+                    <Text style={styles.HoraData}>{horaData}</Text>
+                
+                <Button style={styles.Edit_Button} color={'#BE2444'} onPress={()=>Editar(index)}> |Editar |</Button>
+                <Button style={styles.Delete_Button} color={'#BE2444'} onPress={()=>Excluir(index)}>Feita|</Button>
+                 </TouchableOpacity>
+                })
+
+                }
+            </ScrollView>
+       
         </View>
-    </SafeAreaView>
-);
-};
-const styles = StyleSheet.create({
-    actionICON: {
-        height: 25,
-        width: 25,
-        backgroundColor: COLORS.white,
-        backgroundColor: '#BE2444',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginLeft: 5,
-        borderRadius: 5,
+    );
+    }
+
+    const styles = StyleSheet.create({
+        Tarefa_Container: {
+            flex: 1,
+            backgroundColor: 'white',
+        },
+
+        //header
+        Tarefa_Header: {
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: 10,
+        },
+
+        Tarefa_Header_Text: {
+            fontWeight: 'bold',
+            fontSize: 37,
+            color: '#BE2444',
+            textShadowColor: 'rgba(0, 0, 0, 0.55)',
+            textShadowOffset: {width: -1, height: 1.2},
+            textShadowRadius: 10,
+        },
 
 
-    },
-    listItem: {
-        padding: 20,
-        backgroundColor: COLORS.white,
-        flexDirection: 'row',
-        elevation: 12,
-        borderRadius: 7,
-        marginVertical: 10,
-        backgroundColor: '#BE2444',
 
-    },
-    header: {
-        padding: 20,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
+        //body
+        Tarefa_Body: {
+            flex: 8,
+            padding: 10,
+        },
 
-    },
-    footer: {
-        padding: 20,
-        bottom: 0,
-        position: 'absolute',
-        backgroundColor: COLORS.white,
-        width: '100%',
-        flexDirection: 'row',
-        alignItems: 'center',
-        paddingHorizontal: 20,
-
-    },
-    inputContainer: {
-        backgroundColor: COLORS.white,
-        elevation: 20,
-        flex: 1,
-        height: 50,
-        marginVertical: 20,
-        marginRight: 20,
-        borderRadius: 30,
-        paddingHorizontal: 20,
-    },
-    ICONContainer: {
-        height: 50,
-        width: 50,
-        elevation: 40,
-        height: 50,
-        borderRadius: 30,
-        backgroundColor: COLORS.primary,
-        alignItems: 'center'
+        Body: {
+            flexDirection: 'row',
+            marginBottom: 20,
+        },
 
 
-    },
-});
 
-export default ToDo;
+//Rodapé
+Tarefa_Footer: {
+    flexDirection: 'row',
+    backgroundColor: '#d3d3d3',
+    marginTop: 20,
+    padding: 10,
+    borderRadius: 5,
+    fontSize: 15,
+    margin:5,
+},
+
+Text: {
+    width: '80%',
+    paddingBottom: 5,
+    textTransform: 'capitalize',
+    
+},
+
+HoraData: {
+    position: 'absolute',
+    left: 10,
+    bottom: 1,
+    fontSize: 9,
+},
+
+
+Edit_Button: {
+    position: 'absolute',
+    right: 50,
+},
+
+Delete_Button: {
+    position: 'absolute',
+    right: 0,
+},
+
+    })
